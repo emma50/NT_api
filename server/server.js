d56b376e756ca8e66c2589438017dc232502b076
Your Name <you@example.com>
2019-08-02 14:46:13 +0100

    Add tests for GET /todos

--- server/server.js
+++ server/server.js
@@ -34,6 +34,29 @@ app.get("/todos", (req, res) => {
       .catch((err) => res.status(400).send(err))
 })
 
+// setup GET /todos/:id route
+app.get("/todos/:id", (req, res) => {
+    let id = req.params.id;
+
+    if(!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    Todo.findById(id)
+      .then((todo) => { 
+          if(!todo) {
+              return res.status(404).send();
+          }
+
+          res.send({todo})
+      })
+      .catch((err) => {
+          res.status(400).send(err)
+      })
+})
+
+
+
 // setup users route
 // app.post("/users", (req, res) => {
 //     // console.log(req.body)
--- server/test/server.test.js
+++ server/test/server.test.js
@@ -5,10 +5,20 @@ const {app} = require("./../server");     // const app = require("./../server").
 const {Todo} = require("./../models/todo");
 const {User} = require("./../models/user");
 
+// we create a dummy todo
+const todos = [{
+    text: "First todo test"
+}, {
+    text: "Second todo test"
+}];
+
 // we run a lifecycle/hook method called beforeEach() which allows us to run some code before every single test case
 beforeEach((done) => {
     Todo.remove({})    // wipes all of our Todos
-      .then(() => done())   // only moves to the test case once we call done()
+      .then(() => {    
+         return  Todo.insertMany(todos)
+      })
+      .then(() => done())   // only moves to the test case once we call done() --- an expression syntax
       .catch((err) => console.log("Could not empty the Todo collection", err))
 })
 
@@ -30,7 +40,8 @@ describe("POST /todos", () => {
                  return done(err);    // end test
               }
               
-              Todo.find()  // we want to fetch everything in the Todo collection
+            //   Todo.find()  // fetch everything in the Todo collection
+              Todo.find({text})   // fetch a single Todo
                 .then((todos) => {     // start making assertions
                     expect(todos.length).toBe(1);
                     expect(todos[0].text).toBe(text);
@@ -53,10 +64,23 @@ describe("POST /todos", () => {
 
               Todo.find()  // we want to fetch everything in the Todo collection
                 .then((todos) => {     // start making assertions
-                    expect(todos.length).toBe(0);
+                    expect(todos.length).toBe(2);
                     done();
                 })
                 .catch((err) => done(err))
           })          
     })
 })
+
+describe("GET /todos", () => {
+    it("should get all todos", (done) => {    // the two todos we passed into the function
+        request(app)
+          .get("/todos")
+          .expect(200)
+          .expect((res) => {
+              expect(res.body.todos.length).toBe(2);
+          })
+          .end(done)
+    });
+});
+
